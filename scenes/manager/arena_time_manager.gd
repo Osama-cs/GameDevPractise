056bf3b57397c865f6d486982b414d7685f0aa25
Osama <oilyascs@gmail.com>
2024-12-05 22:38:07 +0000

    Made a basic victory screen.
    It has two button, one that resets the game and one that quits out of the game.

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,8 +1,20 @@
 extends Node
 
+@export var victory_screen_scene: PackedScene
+
 #Assiging a node to a varaible when it is ready to be used.
 #Allows us to fetch the timer from here instead of the scene tree.
 @onready var timer = $Timer
 
+
+func _ready():
+	timer.timeout.connect(on_timer_timeout)
+
+
 func get_time_elapsed():
 	return timer.wait_time - timer.time_left
+
+
+func on_timer_timeout():
+	var victory_screen_instance = victory_screen_scene.instantiate()
+	add_child(victory_screen_instance)
