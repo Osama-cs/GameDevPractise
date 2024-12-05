3666005e838466f830e35e4f8799f863cc36fa0b
Osama <oilyascs@gmail.com>
2024-12-05 22:56:20 +0000

    Added the defeat screen.
    Made the victory and defeat screen into one scene calling into the end screen.

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,6 +1,6 @@
 extends Node
 
-@export var victory_screen_scene: PackedScene
+@export var end_screen_scene: PackedScene
 
 #Assiging a node to a varaible when it is ready to be used.
 #Allows us to fetch the timer from here instead of the scene tree.
@@ -16,5 +16,5 @@ func get_time_elapsed():
 
 
 func on_timer_timeout():
-	var victory_screen_instance = victory_screen_scene.instantiate()
-	add_child(victory_screen_instance)
+	var end_screen_instance = end_screen_scene.instantiate()
+	add_child(end_screen_instance)
