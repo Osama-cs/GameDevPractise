dfd1813b18fac671b11d78dce3bb9c61fe892ec3
Osama <oilyascs@gmail.com>
2024-12-10 16:43:53 +0000

    Made it so that more enemies spawn over time.

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,5 +1,9 @@
 extends Node
 
+signal arena_difficulty_increased(arena_difficulty: int)
+
+const DIFFICULTY_INTERVAL = 5
+
 @export var end_screen_scene: PackedScene
 
 #Assiging a node to a varaible when it is ready to be used.
@@ -7,8 +11,17 @@ extends Node
 @onready var timer = $Timer
 
 
+var arena_difficulty = 0
+
+
 func _ready():
 	timer.timeout.connect(on_timer_timeout)
+
+func _process(delta):
+	var next_time_target = timer.wait_time - ((arena_difficulty + 1) * DIFFICULTY_INTERVAL)
+	if timer.time_left <= next_time_target:
+		arena_difficulty += 1
+		arena_difficulty_increased.emit(arena_difficulty)
 
 
 func get_time_elapsed():
--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -3,20 +3,28 @@ extends Node
 const SPAWN_RADIUS = 375
 
 @export var basic_enemy_scene: PackedScene
+@export var arena_time_manager: Node
+
+@onready var timer = $Timer
+
+var base_spawn_time = 0
 
 
 func _ready():
-	$Timer.timeout.connect(on_timer_timeout)
+	base_spawn_time = timer.wait_time
+	timer.timeout.connect(on_timer_timeout)
+	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
 
 
 func on_timer_timeout():
+	timer.start()
 	# Grab the player, and if the player is null we return null.
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if player == null:
 		return
 	
 	#We get a random diection by taking th right diection, which is at 0 degrees.
-	#Then we rotate it by a random valye between 0 and 2 Pi.
+	#Then we rotate it by a random value between 0 and 2 Pi.
 	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
 	#Next we're going to take the player postion, add it to the random diection times the spawn radius
 	#and that will give the new spawn position.
@@ -29,3 +37,11 @@ func on_timer_timeout():
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 	entities_layer.add_child(enemy)
 	enemy.global_position = spawn_position
+
+
+func on_arena_difficulty_increased(arena_difficulty: int):
+	#There is 12 5 seconds segments in a min so that is why 12 is used here.
+	var time_off = (0.1 / 12) * arena_difficulty
+	time_off = min(time_off, 0.7)
+	print(time_off)
+	timer.wait_time = base_spawn_time - time_off
