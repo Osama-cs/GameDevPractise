b0c6521bcffa87d8565775c64a6e02e9a966a74b
Osama <oilyascs@gmail.com>
2024-12-18 15:48:43 +0000

    You can pick the axe ability on the upgrade menu.

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -10,16 +10,6 @@ func _ready():
 	experience_manager.level_up.connect(on_level_up)
 	
 
-func on_level_up(current_level: int):
-	var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
-	if chosen_upgrade == null:
-		return
-		
-	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
-	add_child(upgrade_screen_instance)
-	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
-	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
-
 
 func apply_upgrade(upgrade: AbilityUpgrade):
 	var has_upgrade = current_upgrades.has(upgrade.id)
@@ -34,5 +24,24 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 	GameEvents.emit_ability_upgrade_added(upgrade, current_upgrades)
 
 
+func pick_upgrades():
+	var chosen_upgardes: Array[AbilityUpgrade] = []
+	var filtered_upgrades = upgrade_pool.duplicate()
+	for i in 2:
+		var chosen_upgrade = filtered_upgrades.pick_random() as AbilityUpgrade
+		chosen_upgardes.append(chosen_upgrade)
+		filtered_upgrades = filtered_upgrades.filter(func (upgrade): return upgrade.id != chosen_upgrade.id)
+	
+	return chosen_upgardes
+
+
 func on_upgrade_selected(upgrade: AbilityUpgrade):
 	apply_upgrade(upgrade)
+
+
+func on_level_up(current_level: int):
+	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
+	add_child(upgrade_screen_instance)
+	var chosen_upgardes = pick_upgrades()
+	upgrade_screen_instance.set_ability_upgrades(chosen_upgardes as Array[AbilityUpgrade])
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
