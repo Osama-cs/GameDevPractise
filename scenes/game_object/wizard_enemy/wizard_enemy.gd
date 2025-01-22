420edc97a1e07ae8a27835c4404ef876cb66fb3c
Osama <oilyascs@gmail.com>
2025-01-22 14:17:07 +0000

    Created the wizard enemy

--- scenes/game_object/wizard_enemy/wizard_enemy.gd
+++ scenes/game_object/wizard_enemy/wizard_enemy.gd
@@ -1,8 +1,7 @@
 extends CharacterBody2D
 
-@onready var visuals = $Visuals 
 @onready var velocity_component = $VelocityComponent
-
+@onready var visuals = $Visuals
 
 func _process(delta):
 	velocity_component.accelerate_to_player()
@@ -10,4 +9,4 @@ func _process(delta):
 	
 	var move_sign = sign(velocity.x)
 	if move_sign != 0:
-		visuals.scale = Vector2(-move_sign, 1)
+		visuals.scale = Vector2(move_sign, 1)
