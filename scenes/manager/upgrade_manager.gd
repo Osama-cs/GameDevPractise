4f2147e2ba09f99cfb19aa5547b8ba85b880a5a5
Osama <oilyascs@gmail.com>
2025-02-28 16:31:29 +0000

    Added multiple different upgrade abilities
    Added sword damage and axe damage. Also made a weighted table to make adding these abilities a lot easier.

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -1,12 +1,22 @@
 extends Node
 
-@export var upgrade_pool: Array[AbilityUpgrade]
+#@export var upgrade_pool: Array[AbilityUpgrade]
 @export var experience_manager: Node
 @export var upgrade_screen_scene: PackedScene
 
 var current_upgrades = {}
+var upgrade_pool: WeightedTable = WeightedTable.new()
+
+var upgrade_axe = preload("res://resources/upgrades/axe.tres")
+var upgrade_axe_damage = preload("res://resources/upgrades/axe_damage.tres")
+var upgrade_sword_rate = preload("res://resources/upgrades/sword_rate.tres")
+var upgrade_sword_damage = preload("res://resources/upgrades/sword_damage.tres")
 
 func _ready():
+	upgrade_pool.add_item(upgrade_axe, 10)
+	upgrade_pool.add_item(upgrade_sword_rate, 10)
+	upgrade_pool.add_item(upgrade_sword_damage, 10)
+	
 	experience_manager.level_up.connect(on_level_up)
 
 func apply_upgrade(upgrade: AbilityUpgrade):
@@ -22,22 +32,27 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 	if upgrade.max_quantity > 0:
 		var current_quantity = current_upgrades[upgrade.id]["quantity"]
 		if current_quantity == upgrade.max_quantity:
-			upgrade_pool = upgrade_pool.filter(func (pool_upgrade): return pool_upgrade.id != upgrade.id)
+			upgrade_pool.remove_item(upgrade)
 	
+	update_upgrade_pool(upgrade)
 	GameEvents.emit_ability_upgrade_added(upgrade, current_upgrades)
+
+func update_upgrade_pool(chosen_upgrade: AbilityUpgrade):
+	if chosen_upgrade.id == upgrade_axe.id:
+		upgrade_pool.add_item(upgrade_axe_damage, 10)
 
 
 func pick_upgrades():
-	var chosen_upgardes: Array[AbilityUpgrade] = []
-	var filtered_upgrades = upgrade_pool.duplicate()
+	var chosen_upgrades: Array[AbilityUpgrade] = []
 	for i in 2:
-		if filtered_upgrades.size() == 0:
+		#if the exclude pool in the chosen upgrades is the same as the overall upgrade pool then we all the items will be excluded
+		# so this check stops that.
+		if upgrade_pool.items.size() == chosen_upgrades.size():
 			break
-		var chosen_upgrade = filtered_upgrades.pick_random() as AbilityUpgrade
-		chosen_upgardes.append(chosen_upgrade)
-		filtered_upgrades = filtered_upgrades.filter(func (upgrade): return upgrade.id != chosen_upgrade.id)
+		var chosen_upgrade = upgrade_pool.pick_item(chosen_upgrades)
+		chosen_upgrades.append(chosen_upgrade)
 	
-	return chosen_upgardes
+	return chosen_upgrades
 
 
 func on_upgrade_selected(upgrade: AbilityUpgrade):
@@ -47,6 +62,6 @@ func on_upgrade_selected(upgrade: AbilityUpgrade):
 func on_level_up(current_level: int):
 	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
 	add_child(upgrade_screen_instance)
-	var chosen_upgardes = pick_upgrades()
-	upgrade_screen_instance.set_ability_upgrades(chosen_upgardes as Array[AbilityUpgrade])
+	var chosen_upgrades = pick_upgrades()
+	upgrade_screen_instance.set_ability_upgrades(chosen_upgrades as Array[AbilityUpgrade])
 	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
