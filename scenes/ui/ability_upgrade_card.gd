1e18b0f009509fa7ccfc3a1949e13abc42d0d016
Osama <oilyascs@gmail.com>
2025-03-11 16:33:57 +0000

    Made a pop in animation for the upgrade card.

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -8,6 +8,20 @@ signal selected
 func _ready():
 	gui_input.connect(on_gui_input)
 
+func play_in(delay: float = 0):
+	# use modulate to hide a UI compoent but keeping it in the layout
+	modulate = Color.TRANSPARENT
+	# we create a timer (this is a scene tree timer, good way to make one off timers)
+	# we pass in a delay to tell the timer to wait a certin amount of time, then timeout
+	# referenecres the timeout signal.
+	# await meanss that we stop the excution of the play_in function, until the timeout
+	# signal has been emited.
+	await get_tree().create_timer(delay).timeout
+	# white here is the upgrade card defualt. this is now in the animation player instead
+	#modulate = Color.WHITE
+	$AnimationPlayer.play("in")
+
+
 func set_ability_upgrade(upgrade: AbilityUpgrade):
 	name_label.text = upgrade.name
 	description_label.text = upgrade.description
--- scenes/ui/upgrade_screen.gd
+++ scenes/ui/upgrade_screen.gd
@@ -10,11 +10,14 @@ func _ready():
 	get_tree().paused = true
 
 func set_ability_upgrades(upgrades: Array[AbilityUpgrade]):
+	var delay = 0
 	for upgrade in upgrades:
 		var card_instance = upgrade_card_scene.instantiate()
 		card_container.add_child(card_instance)
 		card_instance.set_ability_upgrade(upgrade)
+		card_instance.play_in(delay)
 		card_instance.selected.connect(on_upgarde_selected.bind(upgrade))
+		delay += 0.2
 
 
 func on_upgarde_selected(upgrade: AbilityUpgrade):
