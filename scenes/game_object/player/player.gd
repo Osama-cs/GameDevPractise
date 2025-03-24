5c2d66dc9320e8d71b52d1e6f4c0b8dc3bc6fb85
Osama <oilyascs@gmail.com>
2025-03-24 15:13:57 +0000

    Created a player damage effect.

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -70,7 +70,9 @@ func on_body_exited(exited_other_body: Node2D):
 func on_damage_interval_timer_timeout():
 	check_deal_damage()
 
+
 func on_health_changed():
+	GameEvents.emit_player_damaged()
 	update_health_display()
 
 
