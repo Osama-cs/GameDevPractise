90fbcb4c4265a0c57ef9b89cd961526294d20ad1
Osama <oilyascs@gmail.com>
2025-11-19 17:23:00 +0000

    Made the options and quit button functionally.
    This is for making them functionally in the pause menu specifically.

--- scenes/ui/pause_menu.gd
+++ scenes/ui/pause_menu.gd
@@ -2,11 +2,16 @@ extends CanvasLayer
 
 @onready var panel_container = %PanelContainer
 
+var options_menu_scene = preload("res://scenes/ui/options_menu.tscn")
 var is_closing
 
 func _ready():
 	get_tree().paused = true
+	panel_container.pivot_offset = panel_container.size / 2
+	
 	$%ResumeButton.pressed.connect(on_resume_pressed)
+	$%OptionsButton.pressed.connect(on_options_pressed)
+	$%QuitButton.pressed.connect(on_quit_pressed)
 	
 	$AnimationPlayer.play("default")
 	
@@ -26,11 +31,11 @@ func close():
 	
 	is_closing = true
 	
-	$AnimationPlayer.play_backwords("default")
+	$AnimationPlayer.play_backwards("default")
 	
 	var tween = create_tween()
 	tween.tween_property(panel_container, "scale", Vector2.ONE, 0.0)
-	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0.4)\
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0.3)\
 	.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
 	
 	await tween.finished
@@ -40,4 +45,16 @@ func close():
 
 func on_resume_pressed():
 	close()
+
+func on_options_pressed():
+	var options_menu_instance = options_menu_scene.instantiate()
+	add_child(options_menu_instance)
+	options_menu_instance.back_pressed.connect(on_options_back_pressed.bind(options_menu_instance))
+
+func on_options_back_pressed(options_menu: Node):
+	options_menu.queue_free()
+
+func on_quit_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
 
